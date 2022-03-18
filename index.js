e7e36e58a986b731c980e47dc668716839224208
a.lapitskaya <a.lapitskaya@itransition.com>
2022-03-18 16:36:38 +0300

    Corrections after code review

--- index.js
+++ index.js
@@ -3,77 +3,77 @@
 const validationForm = document.querySelector('#form_with_validation');
 const rowsInput = validationForm.querySelector('.rows_input');
 const columnsInput = validationForm.querySelector('.columns_input');
-const fields = validationForm.querySelectorAll('.field');
 
-const generateError = function (text) {
-    let error = document.createElement('div')
+const generateError = (text) => {
+    const error = document.createElement('div')
     error.className = 'error';
     error.innerHTML = text;
     return error;
 };
 
-const highlightTd= function (td) {
-    td.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+const highlightTd = (event) => {
+    const target = event.target;
+    if (/td/i.test(target.tagName)) {
+        target.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    }
+    return
 };
 
-const create_table = function (rowsNumber, columnsNumber) {
-    const table_container = document.querySelector('#table_container');
-    
-    if (table_container.firstChild) {table_container.firstChild.remove()};
+const createTable = (rowsNumber, columnsNumber) => {
+    const tableContainer = document.querySelector('#table_container');
 
-    const new_table = table_container.appendChild(document.createElement('table'));
+    if (tableContainer.firstChild) {
+       (tableContainer.firstChild).removeEventListener('click', highlightTd);
+       tableContainer.innerHTML = null;
+   }
+
+    const newTable = document.createElement('table');
 
     for (let i = 0; i < Math.round(rowsNumber); i++)
     {
-        const main_table_rows =  new_table.insertRow();
+        const mainTableRows =  newTable.insertRow();
 
         for (let j = 0; j < Math.round(columnsNumber); j++)
         {
-            const main_table_cells = main_table_rows.insertCell();
+            mainTableRows.insertCell();
         }
     }
 
-    new_table.addEventListener('click', function(event) {
-        let target = event.target;
+    tableContainer.appendChild(newTable);
 
-        if (target.tagName != 'TD') return;
+    newTable.addEventListener('click', highlightTd);
+};
 
-        highlightTd(target);
+const checkFields = (fieldsToCheck) => {
+    let error;
+    fieldsToCheck.forEach((field) => {
+        let fieldValue = Number(field.value);
+
+        if (!field.value) {
+            error = generateError('Cant be blank');
+        } else if (isNaN(fieldValue)) {
+            error = generateError('Put the number');
+        } else if (fieldValue <= 0) {
+            error = generateError('Put the positive number only');
+        }
+
+        if (error) {
+            field.parentElement.appendChild(error);
+        }
     });
 };
 
-var checkFields = function () {
-    for (let i = 0; i < fields.length; i++) {
-        if (!fields[i].value) {
-            const error = generateError('Cant be blank')
-            fields[i].parentElement.appendChild(error);
-        } else if (fields[i].value <= 0) {
-            const error = generateError('Put the positive number only')
-            fields[i].parentElement.appendChild(error);
-        } else if (isNaN(fields[i].value)) {
-            const error = generateError('Put the number')
-            fields[i].parentElement.appendChild(error);
-        }
-    };
+const removeValidation = (errorsToRemove) => {
+    errorsToRemove.forEach(error => {error.remove()});
 };
 
-var removeValidation = function () {
-    var errors = validationForm.querySelectorAll('.error');
-
-    for (var i = 0; i < errors.length; i++) {
-        errors[i].remove();
-    }
-};
-
-validationForm.addEventListener('submit', function (event) {
+validationForm.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    removeValidation();
+    const errors = validationForm.querySelectorAll('.error');
+    removeValidation(errors);
 
-    checkFields();
-
-    create_table(rowsInput.value, columnsInput.value);
+    const fields = validationForm.querySelectorAll('.field');
+    checkFields(fields);
+    createTable(rowsInput.value, columnsInput.value);
 });
-
-/*
-https://github.com/ALapitskaya/JSChallenge*/
